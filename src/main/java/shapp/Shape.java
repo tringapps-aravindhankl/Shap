398f7ee4b7c601bf027ba6bf0801e4ea6394d524
tringapps-aravindhankl <123533867+tringapps-aravindhankl@users.noreply.github.com>
2023-02-26 00:42:12 +0530

    Update Shape.java

--- src/main/java/shapp/Shape.java
+++ src/main/java/shapp/Shape.java
@@ -1,19 +1,19 @@
-package shapp;
+package shape;
 import java.util.*;
 import java.util.logging.*;
 class Shap {
-    float length;
     float width;
     float radius;
     float length1;
+    float length2;
     String type;
     Logger log=Logger.getLogger("hi");
     // triangle
-    Shap(String type, float width, float length, float length1) {
+    Shap(String type, float width, float length1, float length2) {
         this.type = type;
-        this.length = length;
         this.width = width;
         this.length1 = length1;
+        this.length2 = length2;
 
     }
 
@@ -27,28 +27,28 @@ class Shap {
     Shap(String type, float width, float length) {
         this.type = type;
         this.width = width;
-        this.length = length;
+        this.length1 = length;
     }
 
     void area() {
         // triangle
         if ("TRIANGLe".equalsIgnoreCase(type)) {
-            float area = width * length;  
+            float area = width * length1;  
             area = area / 2;
-            log.fine("area of triangle is { }" area);
+            log.info("area of triangle is" + area);
         }
 
         // circle
         else if ("Circle".equalsIgnoreCase(type)) {
             float area1 = radius * radius;
             area1 = area1 * 3.14f;
-            log.fine("area of circle is {} " area1);
+            log.info("area of circle is" + area1);
         }
 
         // rectangle
         else if ("ReCtangle".equalsIgnoreCase(type)) {
-            float area2 = width * length;
-            log.fine("area of rectangle is {} " area2);
+            float area2 = width * length1;
+            log.info("area of rectangle is" + area2);
         }
 
     }
@@ -56,63 +56,69 @@ class Shap {
     void perimeter() {
         // rectangle
         if ("RectanGle".equalsIgnoreCase(type)) {
-            double perimeter1 = 2 * length * width;
-            log.fine("Perimeter of rectangle is {} "  perimeter1);
+            double perimeter1 = 2 * length1 * width;
+            log.info("Perimeter of rectangle is" + perimeter1);
         }
 
         // circle
         else if ("cirCle".equalsIgnoreCase(type)) {
             double perimeter2 = 2 * 3.14f * radius;
-            log.fine("Perimeter of circle is {} " + perimeter2);
+            log.info("Perimeter of circle is" + perimeter2);
         }
 
         // triangle
         else if ("TRIAnGLE".equalsIgnoreCase(type)) {
-            double perimeter3 = width + length + length1;
-            log.fine("Perimeter of trianlge is {}" + perimeter3);
+            double perimeter3 = width + length1 + length2;
+            log.info("Perimeter of trianlge is" + perimeter3);
         }
     }
 }
+
 
 public class Shape
 {
 	 private static Scanner scan;
 		public static void main( String[] args){
 	    	int option;
-		Logger log;
+		    Logger log;
 	        log=Logger.getLogger("hi");
 	        scan = new Scanner(System.in);
+	        try{
 	        do {
 	            log.info("Enter your option 1.Triangle 2.circle 3.Rectangle 4.Exit");
 	            option = scan.nextInt();
 	            if (option == 1) {
 	                log.info("ENTER WIDTH");
 	                float width = scan.nextFloat();
-	                log.info("Enter length");
-	                float length = scan.nextFloat();
 	                log.info("Enter length1");
 	                float length1 = scan.nextFloat();
-	                Shap a = new Shap("TRIANGLE", width, length, length1);
-	                a.area();
-	                a.perimeter();
+	                log.info("Enter length2");
+	                float length2 = scan.nextFloat();
+	                Shap triangle = new Shap("TRIANGLE", width, length1, length2);
+	                triangle.area();
+	                triangle.perimeter();
 	            } else if (option == 2) {
 	                log.info("Enter radius");
 	                float rad = scan.nextFloat();
-	                Shap b = new Shap("circle", rad);
-	                b.area();
-	                b.perimeter();
+	                Shap Circle = new Shap("circle", rad);
+	                Circle.area();
+	                Circle.perimeter();
 	            } else if (option == 3) {
 	                log.info("Enter width");
 	                float width = scan.nextFloat();
 	                log.info("Enter length");
-	                float length = scan.nextFloat();
-	                Shap c = new Shap("Rectangle", width, length);
-	                c.area();
-	                c.perimeter();
+	                float length1 = scan.nextFloat();
+	                Shap rectangle = new Shap("Rectangle", width, length1);
+	                rectangle.area();
+	                rectangle.perimeter();
 	            }else if(option==4) {
 	            	log.info("Exit Successfully");
 	            }
 	        } while (option < 4);
-        
+	        }
+	        catch(InputMismatchException e) {
+	        	log.info(String.valueOf(e));
+	        	System.exit(0);
+	        }
     }
 }
